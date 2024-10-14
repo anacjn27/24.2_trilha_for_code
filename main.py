165049bc3f913d7dc86336eea9f678746c49611b
Ana de Jesus <anacjn27@eq.ufrj.br>
2024-10-14 18:59:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+falta_pao = True
+falta_mel = False
+
+if falta_mel and falta_pao:
+    print('AND: Precisamos ir ao mercado')
+
+if falta_mel or falta_pao:
+    print('OR: Precisamos ir ao mercado')
