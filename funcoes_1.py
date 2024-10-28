eb76b7cb9262c6cefd3fac3b5c6ce3664a9c1bf0
Ana de Jesus <anacjn27@eq.ufrj.br>
2024-10-28 02:44:23 +0000

    introduzindo funções facultativas

--- funcoes_1.py
+++ funcoes_1.py
@@ -3,5 +3,20 @@
 def soma(x, y):
     var_soma = x + y
     return var_soma
-# chamar a função soma
+
+ #chamar a função soma
 print(soma(x=10, y=5))
+
+# new (se o número for igual a (qualquer número ), dividido por 2 ele vai dar resto 0? se sim, true é par)
+
+def ehpar(numero):
+    return numero % 2 == 0
+
+print(ehpar(numero=10))
+
+# função com parâmetro facultativo
+
+def soma(x, y=10):
+    var_soma = x - y
+    return var_soma
+print(soma(x=10))
